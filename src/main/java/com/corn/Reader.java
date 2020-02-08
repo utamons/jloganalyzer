fb0484da62566b4898c022741e85530b7baf6748
Oleg Zaidullin <cornknight@gmail.com>
2020-02-08 15:18:53 +0600

    Sorting files list by date

--- src/main/java/com/corn/Reader.java
+++ src/main/java/com/corn/Reader.java
@@ -2,7 +2,10 @@ package com.corn;
 
 import org.apache.commons.cli.ParseException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
@@ -21,33 +24,46 @@ public class Reader {
 
 	public void go() {
 		if (files.size() > 1) {
+			System.out.println(files);
 			files.sort(Comparator.comparing(this::getFirstDate));
 			System.out.println(files);
 		}
 	}
 
-	@SuppressWarnings("UnnecessaryContinue")
 	private Instant getFirstDate(File file) {
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 			String line;
+
 			while ((line = br.readLine()) != null) {
-				String[]      parts   = line.split(" ");
-				StringBuilder dateStr = new StringBuilder();
-				for (int i = 0; i < parameters.getDateFmt().split(" ").length; ++i) {
-					dateStr.append(parts[i]);
-				}
-				try {
-					return parameters.parseDate(dateStr.toString());
-				} catch (ParseException e) {
-					continue;
-				}
+				Instant date = getInstant(line);
+				if (date != null)
+					return date;
 			}
-			System.err.println("File "+file.getName()+" doesn't contain dates in format "+parameters.getDateFmt());
+
+			System.err.println("File " + file.getName() + " doesn't contain dates in format " + parameters.getDateFmt());
 			return Instant.now();
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 			return Instant.now();
 		}
+	}
+
+	private Instant getInstant(String line) {
+		final int     dateArrayLength = parameters.getDateFmt().split(" ").length;
+		String[]      parts           = line.split(" ");
+		StringBuilder dateStr         = new StringBuilder();
+		if (parts.length >= dateArrayLength) {
+			for (int i = 0; i < dateArrayLength; ++i) {
+				dateStr.append(parts[i]).append(" ");
+			}
+			dateStr.setLength(dateStr.length() - 1);
+			try {
+				return parameters.parseDate(dateStr.toString());
+			} catch (ParseException e) {
+				return null;
+			}
+		}
+		return null;
 	}
 }
 
