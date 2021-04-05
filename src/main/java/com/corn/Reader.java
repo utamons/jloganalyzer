9d798c0a96e23465aadecf894b7f397382246c59
Oleg Zaidullin <cornknight@gmail.com>
2021-04-05 21:50:27 +0600

    Added coplyleft notice

--- src/main/java/com/corn/Reader.java
+++ src/main/java/com/corn/Reader.java
@@ -1,4 +1,20 @@
 package com.corn;
+/*
+ *     This file is part of JLoganalyzer.
+ *
+ *     JLoganalyzer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     JLoganalyzer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with JLoganalyzer.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import org.apache.commons.cli.ParseException;
 
@@ -10,7 +26,7 @@ import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
 
-/**
+ /**
  * @author Oleg Z. (cornknight@gmail.com)
  */
 public class Reader {
